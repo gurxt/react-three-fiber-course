ad989537f5621794c061a15a9adc8402e1ee0f56
gurxt <jung_jungian@outlook.com>
2023-06-21 09:23:56 -0400

    more memoization

--- src/Box.jsx
+++ src/Box.jsx
@@ -3,19 +3,19 @@ import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
 
 const Box = ({ position, name }) => {
-  const [rotate, setRotate] = useState(false)
   const ref = useRef()
-  const geometry = useMemo(() => new THREE.BoxGeometry(), [])
+  const [count, setCount] = useState(0)
+  const geometry = useMemo(
+    () => [new THREE.BoxGeometry(), new THREE.SphereGeometry(0.65)], []
+  )
 
   useEffect(() => {
     console.log(ref.current.geometry.uuid)
-  }, [rotate])
+  })
 
   useFrame((_, delta) => {
-    if (rotate) {
-      ref.current.rotation.x += delta * rotate
-      ref.current.rotation.y += delta * rotate
-    }
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += 0.5 * delta
   })
 
   return (
@@ -23,10 +23,9 @@ const Box = ({ position, name }) => {
       ref={ref}
       position={position}
       name={name}
-      geometry={geometry}
-      onPointerDown={() => setRotate(!rotate)}
+      geometry={geometry[count]}
+      onPointerDown={() => setCount((count + 1) % 2)}
     > 
-      <boxGeometry />
       <meshBasicMaterial color='red' wireframe />
     </mesh>
   )
