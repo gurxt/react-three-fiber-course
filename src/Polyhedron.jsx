6426150d59846f9bf08a577a082632c49d0a7494
gurxt <jung_jungian@outlook.com>
2023-06-21 14:37:43 -0400

    mesh facing

--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -4,14 +4,10 @@ import { useFrame } from '@react-three/fiber'
 export default function Polyhedron(props) {
   const ref = useRef()
 
-  useFrame((_, delta) => {
-    ref.current.rotation.x += 0.2 * delta
-    ref.current.rotation.y += 0.05 * delta
-  })
 
   return (
     <mesh {...props} ref={ref} castShadow receiveShadow>
-      <icosahedronGeometry args={[1, 1]} />
+      <boxGeometry args={[1.25, 2, 0.10]} />
     </mesh>
   )
 }
