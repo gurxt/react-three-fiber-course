4dfac5a97a677ceae01ca5629f4873084833a069
gurxt <jung_jungian@outlook.com>
2023-06-21 08:46:27 -0400

    working with useFrame

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useRef } from 'react'
+import { useFrame } from '@react-three/fiber'
+
 const Box = ({ position, name }) => {
+  const ref = useRef()
+
+  // state can be imported instead of _
+  useFrame((_, delta) => {
+    ref.current.rotation.x += 1 * delta
+    ref.current.rotation.y += 0.5 * delta
+    //ref.current.position.y = Math.sin(state.clock.getElapsedTime() * 4 / 2)
+  })
+
+  useEffect(() => {
+    console.log(ref) 
+  }, [])
+
   return (
-    <mesh position={position} name={name}>
+    <mesh ref={ref} position={position} name={name}>
       <boxGeometry />
       <meshBasicMaterial color="red" wireframe />
     </mesh>
