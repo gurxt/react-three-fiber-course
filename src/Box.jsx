8599770906935d3f38adc738dcea7cfa3e2e2369
gurxt <jung_jungian@outlook.com>
2023-06-21 08:55:42 -0400

    working with mesh event props

--- src/Box.jsx
+++ src/Box.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react'
+import { /*useEffect*/ useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 
 const Box = ({ position, name }) => {
@@ -11,12 +11,23 @@ const Box = ({ position, name }) => {
     //ref.current.position.y = Math.sin(state.clock.getElapsedTime() * 4 / 2)
   })
 
+  /*
   useEffect(() => {
     console.log(ref) 
   }, [])
+  */
 
   return (
-    <mesh ref={ref} position={position} name={name}>
+    <mesh 
+      ref={ref} 
+      position={position}
+      name={name}
+      onPointerDown={(e) => console.log(`Pointer down: ${e.object.name}`)}
+      onPointerUp={(e) => console.log(`Pointer up: ${e.object.name}`)}
+      onPointerOver={(e) => console.log(`Pointer over: ${e.object.name}`)}
+      onPointerOut={(e) => console.log(`Pointer out: ${e.object.name}`)}
+      onUpdate={(e) => console.log(e)}
+    >
       <boxGeometry />
       <meshBasicMaterial color="red" wireframe />
     </mesh>
