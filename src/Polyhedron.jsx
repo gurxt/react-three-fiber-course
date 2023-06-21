a0239991fe1633d64e597216bda2fe9a5f4c452b
gurxt <jung_jungian@outlook.com>
2023-06-21 13:56:41 -0400

    lights

--- src/Polyhedron.jsx
+++ src/Polyhedron.jsx
@@ -1,7 +1,5 @@
 
 import { useRef } from 'react'
-import { useControls } from 'leva'
-import * as THREE from 'three'
 import { useFrame } from '@react-three/fiber'
 
 export default function Polyhedron(props) {
@@ -10,28 +8,6 @@ export default function Polyhedron(props) {
   useFrame((_, delta) => {
     ref.current.rotation.x += 0.2 * delta
     ref.current.rotation.y += 0.05 * delta
-  })
-
-  useControls(props.name, {
-    wireframe: {
-      value: false,
-      onChange: (v) => {
-        ref.current.material.wireframe = v
-      },
-    },
-    flatShading: {
-      value: true,
-      onChange: (v) => {
-        ref.current.material.flatShading = v
-        ref.current.material.needsUpdate = true
-      },
-    },
-    color: {
-      value: 'lime',
-      onChange: (v) => {
-        ref.current.material.color = new THREE.Color(v)
-      },
-    },
   })
 
   return (
