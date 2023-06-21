b9d0f98e65c35a2ab8b2be78027fb141e51c59db
gurxt <jung_jungian@outlook.com>
2023-06-21 10:41:36 -0400

    add drei + display stats

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import Polyhedron from './Polyhedron'
 import * as THREE from 'three'
+import { Stats } from '@react-three/drei'
 
 const App = () => {
   const polyhedron = [
@@ -15,6 +16,7 @@ const App = () => {
       <Polyhedron position={[0.75, -0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[-0.75, 0.75, 0]} polyhedron={polyhedron} />
       <Polyhedron position={[0.75, 0.75, 0]} polyhedron={polyhedron} />
+      <Stats />
     </Canvas>
   )
 }
